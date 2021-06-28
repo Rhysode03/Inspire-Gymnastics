8aea8080e28be77122cbf85b7f8b651d1c5e6154
Kyle Calder <kylebcalder03@gmail.com>
2021-06-28 11:14:41 -0600

    Added styling to mobile table

--- js/tableToCards.js
+++ js/tableToCards.js
@@ -99,10 +99,10 @@ function tableToCards() {
 		// Generate cards
 		var cards = [];
 		$(this).find("tbody tr").each(function() {
-			var cardTitle = $("<h5>").addClass("card-title");
+			var cardTitle = $("<h5>").addClass("card-title text-primary");
 			var cardSubtitles = [];
 			var cardText = $("<p>").addClass("card-text");
-			var cardFooters = $("<div>").addClass("card-footer text-muted");
+			var cardFooters = $("<div>").addClass("card-footer bg-success");
 			var actionLinks = [];
 
 			var appliedFormats = [];
